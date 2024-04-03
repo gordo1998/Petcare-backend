d79ab6c459f0fd80100ce7a6b71bba708ae8e248
gordo1998 <jordibarreda98@gmail.com>
2024-04-03 16:32:02 +0200

    j

--- PetcareV2.sql
+++ PetcareV2.sql
@@ -2,6 +2,7 @@ drop database petcare;
 create database petcare;
 use petcare;
 
+# nada
 create table cuenta
 (
 idCuenta int auto_increment,
@@ -14,35 +15,45 @@ email varchar(100),
 movil int(9),
 telefono int(9),
 urlImagenes varchar(200),
-tipoPerdil boolean,
+tipoPerfil tinyint(1),
 constraint pk_cuenta primary key (idCuenta)
 );
 
+# Debe tener los animales. Debe tener una descripción corta y una larga.
+#  Debe contener un numero que identifique las veces que un usuario se ha conectado. 
+# El numero nos servirá para poder identificar la primera vez que el usuario se conecta como dueño.
 create table dueño
 (
 idDueño int auto_increment,
 descripcion varchar(100),
+accesFirstTime int,
 cuentaDueño int,
 constraint pk_dueño primary key (idDueño),
 constraint fk_dueñoUno foreign key (cuentaDueño) references cuenta(idCuenta)
+on delete cascade on update cascade
 );
 
+# Debe tener los animales que cuida. Debe tener una descripción larga y una corta.
+# Debe tener puntuación.
 create table cuidador
 (
 idCuidador int auto_increment,
 descripcion longtext,
+accesFirstTime int,
 cuentaCuidador int,
 constraint pk_cuidador primary key (idCuidador),
 constraint fk_cuidadorUno foreign key (cuentaCuidador) references cuenta(idCuenta)
+on delete cascade on update cascade
 );
 
+# nada
 create table servicios
 (
 idServicio int auto_increment,
 nombreServicio varchar(100),
 constraint pk_servicio primary key (idServicio)
 );
-
+# Revisar enpoints que devuelvan información que provenga de esta tabla porque la he modificado
 create table servicioCuidador
 (
 idServicio int,
@@ -50,19 +61,99 @@ idCuidador int,
 cuidadorSC int,
 servicioSC int,
 constraint pf_servicioCuidador primary key(idServicio, idCuidador),
-constraint fk_uno foreign key (cuidadorSC) references cuidador(idCuidador),
+constraint fk_uno foreign key (cuidadorSC) references cuidador(idCuidador)
+on delete cascade on update cascade,
 constraint fk_dos foreign key (servicioSC) references servicios(idServicio)
+on delete cascade on update cascade
 );
 
+#Esta tabla contendrá los dias no disponibles de un cuidador para las reservas
+create table DiaNoDisponible
+(
 
+);
+
+# Esta tabla es para guardar los usuarios que un cuidador ha guardado en favoritos
+create table cuidadorFavorito
+(
+idCuidadorFavorito int,
+idDueñoDelFavorito int,
+constraint pk_cuidadorFavorito primary key (idCuidadorFavorito, idDueñoDelFavorito),
+constraint fk_cuidadorFavorito foreign key (idCuidadorFavorito) references cuidador(idCuidador)
+on delete cascade on update cascade,
+constraint fk_dueloDelFavorito foreign key (idDueñoDelFavorito) references dueño(idDueño)
+on delete cascade on update cascade
+);
+
+# Debe tener la fecha y la hora de la reserva. Debe tener la ubicación. Debe tener 
 create table reservas
 (
 idReserva int auto_increment,
 cuidadorR int,
 duenyoR int,
 constraint pk_reserva primary key (idReserva),
-constraint fk_cuidadorR foreign key (cuidadorR) references cuidador(idCuidador),
+constraint fk_cuidadorR foreign key (cuidadorR) references cuidador(idCuidador)
+on delete cascade on update cascade,
 constraint fk_duenyoR foreign key (duenyoR) references dueño(idDueño)
+on delete cascade on update cascade
+);
+
+# Aquí guardaremos el animal
+create table animal
+(
+idAnimal int auto_increment,
+nombre varchar(100) not null,
+constraint pk_animal primary key (idAnimal)
+);
+
+#Aquí guardaremos la raza de los animales
+create table raza
+(
+idRaza int auto_increment,
+nombre varchar(100) not null,
+animalRaza int,
+constraint pk_raza primary key (idRaza),
+constraint fk_animal foreign key (animalRaza) references animal(idAnimal)
+on delete cascade on update cascade
+
+);
+
+create table alimento
+(
+idAlimento int auto_increment,
+nombre varchar(100),
+constraint pk_alimento primary key (idAlimento)
+);
+
+# Aquí guardaremos todas las mascotas que tiene un usuario dueño
+create table mascota
+(
+idMascota int auto_increment,
+nombre varchar(100),
+edad int, 
+peso float,
+descEnfermedades longtext,
+descSobreMascota longtext,
+alimentoMascota int,
+duenyoMascota int,
+razaMascota int,
+constraint pk_mascota primary key (idMascota),
+constraint fk_alimento foreign key (alimentoMascota) references alimento(idAlimento) 
+on delete cascade on update cascade,
+constraint fk_duenyo foreign key (duenyoMascota) references dueño(idDueño)
+on delete cascade on update cascade,
+constraint fk_raza foreign key (razaMascota) references raza(idRaza)
+on delete cascade on update cascade
+);
+
+create table mascotaReservado
+(
+
+);
+
+create table cuidadorAnimal
+(
+
 );
 
 use petcare;
